53da207b2e3d3dc88f483d5d2157993fb81d5349
Ilya Stolbunov <stolb216@yandex.com>
2025-10-25 18:11:06 +0300

    auth ver4

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,8 +2,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
-
+#SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost:5432/hackathon_rtk"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 local_session = sessionmaker(autocommit = False, autoflush=False, bind=engine)
