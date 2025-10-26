c2f96a269bd7a6dd282b5deeff507a6a93ede6ab
Sergey Petrushin <s.petrushin128@gmail.com>
2025-10-26 10:39:49 +0300

    у нас что-то начало получаться))

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.add_middleware(AuditMiddleware)
+app.add_middleware(AuditMiddleware)
 
 app.include_router(projects.router)
 app.include_router(auth.router)
